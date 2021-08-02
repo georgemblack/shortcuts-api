ac3e05adb64f0744a9c2680a3f3cf831cb9be70c
George Black <contact@george.black>
2021-08-01 21:21:46 -0500

    Update validation failure response

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,23 +1,25 @@
+const messages = require("./messages");
+
 function validateShoeboxEntry(req, res, next) {
   const body = req.body;
   if (!body) {
-    return res.status(400).send("Validation failed");
+    return res.status(400).send({ message: messages.sadResponseMessage() });
   }
 
   /**
    * Check geopoint (required attribute)
    */
   if (!("geopoint" in body)) {
-    return res.status(400).send("Validation failed");
+    return res.status(400).send({ message: messages.sadResponseMessage() });
   }
 
   const geopoint = body.geopoint;
   if (!("latitude" in geopoint && "longitude" in geopoint)) {
-    return res.status(400).send("Validation failed");
+    return res.status(400).send({ message: messages.sadResponseMessage() });
   }
 
   if (!(Number(geopoint.latitude) && Number(geopoint.longitude))) {
-    return res.status(400).send("Validation failed");
+    return res.status(400).send({ message: messages.sadResponseMessage() });
   }
 
   /**
@@ -27,7 +29,7 @@ function validateShoeboxEntry(req, res, next) {
     const text = body.text;
 
     if (!text || typeof text !== "string") {
-      return res.status(400).send("Validation failed");
+      return res.status(400).send({ message: messages.sadResponseMessage() });
     }
   }
 
